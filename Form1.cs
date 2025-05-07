e5f4945e0d026fe3a99466cca3443447c8e3ad51
Berkant Gunes <gunesberkant96@gmail.com>
2025-05-07 11:16:05 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // form üzerinde 2 listbox olsun. 1. listbox dan türkiye de bulunan şehirlerden herhangi birini seçtiğimiz de ilgili sehrin ilçeleri 2. listbox da listelensin.
+
+            // textbox a girilen bir keliyi harf olarak listbox a ekleyen programı yazınız.
+
+            // textbox a girilen bir kelimeyi alfabetik olarak sıraladıktan sonra listbox a ekleyen programı yazınız.
+
+            // listbox da seçilen bir nesneyi 2. listbox a tıkladığımız da taşıma işlemi yapan programı yazınız.
+
+        }
+    }
+}
